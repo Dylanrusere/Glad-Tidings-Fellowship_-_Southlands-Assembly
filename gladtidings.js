16c257256475ed16ef0fcd9c403d9c00a08ad417
Dylanrusere <dylaneight01@gmail.com>
2024-05-15 12:11:02 +0200

    first commit

---
+++ gladtidings.js
@@ -0,0 +1,7 @@
+const hamMenu = document.querySelector(".ham_menu");
+const offMenu = document.querySelector(".offScreenMenu");
+
+hamMenu.addEventListener("click", () => {
+    hamMenu.classList.toggle("active");
+    offMenu.classList.toggle("active");
+});
