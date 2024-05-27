ce1b1739898f3d96acea3a939da4d5926ed75c71
Dylanrusere <dylaneight01@gmail.com>
2024-05-27 08:08:48 +0100

    update ministries

--- gladtidings.js
+++ gladtidings.js
@@ -5,3 +5,14 @@ hamMenu.addEventListener("click", () => {
     hamMenu.classList.toggle("active");
     offMenu.classList.toggle("active");
 });
+
+// Carousel
+const slider = document.querySelector('.slider');
+
+function activate(e) {
+  const items = document.querySelectorAll('.item');
+  e.target.matches('.next') && slider.append(items[0])
+  e.target.matches('.prev') && slider.prepend(items[items.length-1]);
+}
+
+document.addEventListener('click',activate,false);
